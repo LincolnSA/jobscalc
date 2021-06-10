0f0228a8b1e26c059d375d5bdf45816ce961e06b
lincoln silva <jllincolnsilva@gmail.com>
2021-06-09 22:09:34 -0300

    Project completed

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const server = express()
+const routes = require("./routes")
+const path = require("path")
+const port = 3000
+
+// usando template engine
+server.set('view engine', 'ejs')
+
+// Mudar a localização da pasta views
+server.set('views', path.join(__dirname, 'views'))
+
+//habilitar arquivos statics
+server.use(express.static("public"))
+
+// usar o req.body
+server.use(express.urlencoded({ extended: true }))
+
+// routes
+server.use(routes)
+
+server.listen(port, () => console.log(`Servidor on http://localhost:${port}`))
